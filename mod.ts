1159507adda4de6cda7043417f7996293b6dd300
迷渡 <justjavac@gmail.com>
2021-03-31 08:41:57 +0800

    添加类型信息
    Signed-off-by: 迷渡 <justjavac@gmail.com>

--- mod.ts
+++ mod.ts
@@ -1,3 +1,5 @@
+/// <reference path="./deployctl.d.ts" />
+
 addEventListener("fetch", (event: FetchEvent) => {
   const response = new Response("Hello World!", {
     headers: { "content-type": "text/plain" },
